4d1f5102e09de60a98f2e1cdf1061ebf977baeab
rubikstriangle <42789620+rubikstriangle@users.noreply.github.com>
2024-05-29 17:32:33 -0700

    Update build_openrv.sh

--- build_openrv.sh
+++ build_openrv.sh
@@ -26,7 +26,7 @@ docker buildx create --use
 docker buildx inspect --bootstrap
 
 # Build Docker image
-docker buildx build --build-arg QT_USER=${QT_USER} --build-arg QT_PASSWORD=${QT_PASSWORD} -t openrv_rocky9 .
+docker buildx build --load --build-arg QT_USER=${QT_USER} --build-arg QT_PASSWORD=${QT_PASSWORD} -t openrv_rocky9 .
 
 # Run the container to copy the tarball
 docker run -d --name openrv_container openrv_rocky9
