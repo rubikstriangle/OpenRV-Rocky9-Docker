84a0dd8b90f2a1f7521e0d2fc490eb78a70e66d0
rubikstriangle <42789620+rubikstriangle@users.noreply.github.com>
2024-06-05 13:39:06 -0700

    Update build_openrv.sh

--- build_openrv.sh
+++ build_openrv.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+
+
+
 # Usage function
 usage() {
     echo "Usage: $0 -u <QT_USER> -p <QT_PASSWORD>"
@@ -26,13 +30,85 @@ if [ "$QT_USER" == "your_qt_username" ] || [ "$QT_PASSWORD" == "your_qt_password
     usage
 fi
 
-# Ensure Buildx is installed
-#docker buildx install
-#docker buildx create --use
-#docker buildx inspect --bootstrap
+# Prompt user for license agreement acceptance
+echo "Qt Open Source version is available under GNU General Public License v3 and Lesser GNU General Public License v3. A few components are available under GNU General Public License v2."
+echo "Read and accept the Open Source Usage Obligations below. Reading the link below helps you choosing the right license for your project."
+echo ""
+echo "Choosing the right license for your projects: https://www.qt.io/licensing"
+echo "Buy Qt: https://www.qt.io/buy-product"
+echo ""
+echo "(Lesser) GNU General Public License v3 obligations:"
+echo ""
+echo "* You must not combine code developed with a commercial Qt license with code developed with an open source license of Qt in one project or product"
+echo "* Provide a license copy & explicitly acknowledge Qt use"
+echo "* Make a Qt source code copy available for customers"
+echo "* Accept that Qt source code modifications are non-proprietary"
+echo "* Make consumer devices, which allow users to access to install and run modified versions of the SW inside them"
+echo "* Accept Digital Rights Management terms, see the GPL FAQ: http://www.gnu.org/licenses/gpl-faq.html#DRMProhibited"
+echo "* Take special consideration when attempting to enforce software patents: https://www.gnu.org/licenses/gpl-faq.html#v3PatentRetaliation"
+echo "* For further information, refer to GPLv3: https://www.gnu.org/licenses/gpl-3.0.html and GPLv3: https://www.gnu.org/licenses/lgpl-3.0.en.html"
+echo ""
+read -p "Accept|Reject: " LICENSE_PROMPT
+
+# Capitalize the first letter of the user's response
+LICENSE_PROMPT="$(tr '[:lower:]' '[:upper:]' <<< ${LICENSE_PROMPT:0:1})${LICENSE_PROMPT:1}"
+
+if [[ "$LICENSE_PROMPT" != "Accept" ]]; then
+    echo "Warning: Open Source obligations not accepted by user. Aborting."
+    exit 1
+fi
+
+# Loop until a valid response is provided for the telemetry question
+while true; do
+    echo "telemetry-question : Contribute to Qt Development : Help us improve Qt and Qt Creator by allowing tracking of pseudonymous usage data in Qt Creator. The tracking can be disabled at any time. Read the Qt Company data collection privacy statement https://www.qt.io/terms-conditions/#privacy."
+    read -p "Yes|No: " TELEMETRY_PROMPT
+
+    # Capitalize the first letter of the user's response
+    TELEMETRY_PROMPT="$(tr '[:lower:]' '[:upper:]' <<< ${TELEMETRY_PROMPT:0:1})${TELEMETRY_PROMPT:1}"
+
+    if [[ "$TELEMETRY_PROMPT" == "Yes" || "$TELEMETRY_PROMPT" == "No" ]]; then
+        break
+    else
+        echo "Invalid answer, please retry."
+    fi
+done
+
+# Create the accept_license.exp script with expect commands
+cat << EOF > accept_license.exp
+#!/usr/bin/expect
+
+set timeout 600
+
+spawn /tmp/qt-installer.run --email ${QT_USER} --root /root/Qt --password ${QT_PASSWORD} --platform minimal --accept-licenses --confirm-command install qt.qt5.5152.qtpdf qt.qt5.5152.qtpurchasing qt.qt5.5152.qtvirtualkeyboard qt.qt5.5152.qtquicktimeline qt.qt5.5152.qtlottie qt.qt5.5152.debug_info qt.qt5.5152.qtscript qt.qt5.5152.qtcharts qt.qt5.5152.qtwebengine qt.qt5.5152.qtwebglplugin qt.qt5.5152.qtnetworkauth qt.qt5.5152.qtwaylandcompositor qt.qt5.5152.qtdatavis3d qt.qt5.5152.logs qt.qt5.5152 qt.qt5.5152.src qt.qt5.5152.gcc_64 qt.qt5.5152.qtquick3d
+
+expect {
+    "Accept|Reject" {
+        send "${LICENSE_PROMPT}\r"
+        exp_continue
+    }
+    "Yes|No" {
+        send "${TELEMETRY_PROMPT}\r"
+        exp_continue
+    }
+    timeout {
+        send_user "Timeout waiting for input\n"
+        exit 1
+    }
+    eof {
+        send_user "End of file reached\n"
+        exit 0
+    }
+    default {
+        exp_continue
+    }
+}
+EOF
+
+# Make the new script executable
+chmod +x accept_license.sh
 
 # Build Docker image
-docker build --load --build-arg QT_USER=${QT_USER} --build-arg QT_PASSWORD=${QT_PASSWORD} -t openrv_rocky9 .
+docker build --load -t openrv_rocky9 .
 
 
 # Verify the target container has been stopped 
